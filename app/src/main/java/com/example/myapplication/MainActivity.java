b4c906836b1073b8a6fd0abf074e3ff419f19c79
shhdarkness2 <47919667+shhdarkness2@users.noreply.github.com>
2019-02-24 12:04:47 +0330

    Update MainActivity.java

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -17,6 +17,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         String s = "test";
         String s1 = "test";
+        String s1 = "test";
+        String s1 = "test";
 
 
 
